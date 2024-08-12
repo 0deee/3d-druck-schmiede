0f2fc37f851a0792465d1695e749545d866d2f6e
0deee <oliver.dankwart@gmail.com>
2024-08-13 00:22:24 +0200

    rss file update

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,13 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+const title = "3D-Druck-Schmiede";
+const description = "3d-Druck und 3D Modeling mit 3D-Druck-Schmiede aus Friedrichshafen";
 
 export async function GET(context) {
 	const posts = await getCollection('blog');
 	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
+		title: title,
+		description: description,
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
